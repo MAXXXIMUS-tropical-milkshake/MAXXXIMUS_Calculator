d2e7068b443194cbc0d8079a757909562469c188
Maksim <mi9992828@gmail.com>
2024-09-22 18:09:35 +0300

    fix: fixed struct History

--- backend/internal/controller/http/model/history/history.go
+++ backend/internal/controller/http/model/history/history.go
@@ -1,16 +1,13 @@
 package history
 
 import (
-	"time"
 	"github.com/MAXXXIMUS-tropical-milkshake/MAXXXIMUS_Calculator/internal/controller/http/model/pagination"
 )
 
 type (
 	History struct {
-		ID         int       `json:"id"`
 		UserID     int       `json:"user_id"`
 		Expression string    `json:"expression"`
-		CreatedAt  time.Time `json:"created_at"`
 	}
 
 	HistoryResponse struct {

e8a06eed3ad38c190dfe5d933861ffe75937ab9b
Vitas <ccloudyy74@gmail.com>
2024-09-25 00:56:26 +0300

    fix(calculator): fixed bug with double ops

--- ui/lib/calculator/cubit/calculator_cubit.dart
+++ ui/lib/calculator/cubit/calculator_cubit.dart
@@ -7,7 +7,22 @@ const _supportedOps = [
   "*",
   "/",
 ];
-const _supportedNonOps = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "00", "."];
+const _supportedNonOps = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "00",
+  "."
+];
+
+bool lastisop = false;
 
 class CalculatorCubit extends Cubit<String> {
   CalculatorCubit() : super("");
@@ -15,20 +30,33 @@ class CalculatorCubit extends Cubit<String> {
   void insertSymbol(String symbol) {
     if (!_supportedNonOps.contains(symbol)) return;
     emit(state + symbol);
+    lastisop = false;
   }
 
   void insertOp(String op) {
-    if (!_supportedOps.contains(op)) return;
+    if (state.isEmpty) return;
+    if (lastisop) {
+        emit(state.substring(0, state.length - 1) + op);
+        return;
+    }
     emit(state + op);
+    lastisop = true;
   }
 
-  void eraseLast() =>
-      emit(state.isEmpty ? state : state.substring(0, state.length - 1));
+  void eraseLast() {
+    emit(state.isEmpty ? state : state.substring(0, state.length - 1));
+    lastisop = false;
+  }
 
-  void eraseAll() =>
-      emit("");
+  void eraseAll() {
+    emit("");
+    lastisop = false;
+  }
 
   Future<void> evaluate() async {
+    if (state.isEmpty) return;
+    if (lastisop) return;
     emit(await CalculatorClient.calculate(state));
+    lastisop = false;
   }
 }

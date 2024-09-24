e8b38d0eff4b43ff6e58a7d970c8d7d444a81fe0
Petrukhin <dartmol2300@gmail.com>
2024-09-24 15:40:57 +0300

    fix: introduce approximate api call

--- ui/lib/calculator/calculator_client.dart
+++ ui/lib/calculator/calculator_client.dart
@@ -3,13 +3,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class CalculatorClient {
+  // TODO: may get non-string as argument
   Future<String> calculate(String expr) async {
     final client = http.Client();
-    final url = Uri.http('localhost:8080', 'handle');
-    final response = await client.post(url, body: expr);
-    final decodedResponse =
-        jsonDecode(utf8.decode(response.bodyBytes)) as String;
+    final url = Uri.http('localhost:8080', 'api/v1/calculate', {"data": expr});
+    final response = await client.get(url);
+    final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
     client.close();
-    return decodedResponse;
+    return decodedResponse["data"]?.toString() ?? ":(";
   }
 }

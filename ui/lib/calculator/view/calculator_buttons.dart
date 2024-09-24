ee7823c63e390d548a6bddff08f1f4a9dd219ff1
Petrukhin <dartmol2300@gmail.com>
2024-09-25 02:15:17 +0300

    refactor: change button themes access, extract multiple buttons, small architecture fix for pretty times op

---
+++ ui/lib/calculator/view/calculator_buttons.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ui/calculator/cubit/calculator_cubit.dart';
+
+class CalculatorButtonStyle {
+  static final _borderShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(50),
+  );
+
+  static const _padding = EdgeInsets.symmetric(vertical: 24, horizontal: 16);
+  static const _textStyle = TextStyle(fontSize: 24);
+
+  static ButtonStyle get digit => ElevatedButton.styleFrom(
+        padding: _padding,
+        textStyle: _textStyle,
+        backgroundColor: const Color.fromARGB(255, 97, 97, 97),
+        foregroundColor: Colors.white,
+        shape: _borderShape,
+      );
+
+  static ButtonStyle get equals => ElevatedButton.styleFrom(
+        padding: _padding,
+        textStyle: _textStyle,
+        backgroundColor: Colors.orange,
+        foregroundColor: Colors.white,
+        shape: _borderShape,
+      );
+
+  static ButtonStyle get clear => ElevatedButton.styleFrom(
+        padding: _padding,
+        textStyle: _textStyle,
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        shape: _borderShape,
+      );
+
+  static ButtonStyle get operation => ElevatedButton.styleFrom(
+        padding: _padding,
+        textStyle: _textStyle,
+        backgroundColor: const Color.fromARGB(255, 26, 23, 23),
+        foregroundColor: Colors.white,
+        shape: _borderShape,
+      );
+
+  static ButtonStyle get history => ElevatedButton.styleFrom(
+        padding: _padding,
+        textStyle: _textStyle,
+        backgroundColor: const Color.fromARGB(255, 26, 23, 23),
+        foregroundColor: Colors.white,
+        shape: _borderShape,
+      );
+}
+
+class CalculatorClearButton extends StatelessWidget {
+  const CalculatorClearButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          style: CalculatorButtonStyle.clear,
+          onPressed: () => context.read<CalculatorCubit>().eraseAll(),
+          child: const Text("С"),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorEqualsButton extends StatelessWidget {
+  const CalculatorEqualsButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          style: CalculatorButtonStyle.equals,
+          onPressed: () => context.read<CalculatorCubit>().evaluate(),
+          child: const Text("="),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorOperationButton extends StatelessWidget {
+  const CalculatorOperationButton({
+    super.key,
+    required this.operation,
+  });
+
+  final String operation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          style: CalculatorButtonStyle.operation,
+          onPressed: () => context.read<CalculatorCubit>().insertOp(operation),
+          child: Text(operation),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorHistoryButton extends StatelessWidget {
+  const CalculatorHistoryButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.center,
+        child: ElevatedButton(
+          style: CalculatorButtonStyle.history,
+          onPressed: () => context.go("/history"),
+          child: const Text("History"),
+        ));
+  }
+}
+
+class CalculatorDigitButton extends StatelessWidget {
+  const CalculatorDigitButton({super.key, required this.digit});
+
+  final String digit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          style: CalculatorButtonStyle.digit,
+          onPressed: () => context.read<CalculatorCubit>().insertSymbol(digit),
+          child: Text(digit),
+        ),
+      ),
+    );
+  }
+}
--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -3,19 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ui/app.dart';
 import 'package:ui/calculator_observer.dart';
 import 'dart:io';
-
+import 'package:flutter/foundation.dart';
 import 'package:window_size/window_size.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const CalculatorObserver();
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
     setWindowTitle('Maxxximus Calculator');
-    
+
     setWindowMinSize(const Size(600, 800));
     setWindowMaxSize(const Size(600, 800));
 
-    setWindowFrame(Rect.fromLTWH(100, 100, 600, 800));
+    setWindowFrame(const Rect.fromLTWH(100, 100, 600, 800));
   }
   runApp(CalculatorApp());
 }

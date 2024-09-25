a0b33e45b7deaf69ae3fca0dc99894979bdf7619
Petrukhin <dartmol2300@gmail.com>
2024-09-25 06:05:37 +0300

    feat: introduce auth and skeleton for history fetch

--- ui/lib/calculator/calculator_client.dart
+++ ui/lib/calculator/calculator_client.dart
@@ -2,7 +2,28 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+class Auth {
+  static String _token = "";
+
+  static Future<String> get token async {
+    if (_token != "") return _token;
+    _token = await CalculatorClient.auth();
+    return _token;
+  }
+}
+
 class CalculatorClient {
+  static Future<String> auth() async {
+    final client = http.Client();
+    final url = Uri.http('localhost:8080', 'api/v1/auth/signup');
+    final response = await client.get(url);
+    final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+    client.close();
+    return decodedResponse["status"] == "success"
+        ? decodedResponse["data"]
+        : "";
+  }
+
   static Future<int?> calculate(String expr) async {
     final client = http.Client();
     final url = Uri.http('localhost:8080', 'api/v1/calculate', {"data": expr});
@@ -19,13 +40,16 @@ class CalculatorClient {
     final url = Uri.http('localhost:8080', 'api/v1/history', {"limit": "10"});
     // ignore: avoid_print
     print("sent history request");
-    final response = await client.get(url);
+    final response =
+        await client.get(url, headers: {"Authorization": await Auth.token});
     // ignore: avoid_print
     print("got history response");
     final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
     client.close();
-    return (decodedResponse["data"]["history"] as List<Map>)
-        .map((e) => e["expression"] as String)
-        .toList();
+    return decodedResponse["status"] == "success"
+        ? (decodedResponse["data"]["history"] as List<Map>)
+            .map((e) => e["expression"] as String)
+            .toList()
+        : [];
   }
 }
--- ui/lib/calculator/view/history_page.dart
+++ ui/lib/calculator/view/history_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:ui/calculator/cubit/history_cubit.dart';
 import 'package:ui/calculator/view/calculator_buttons.dart';
 
